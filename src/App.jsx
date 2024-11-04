f515f86e7d734726334683d7df8a1634f8cca253
Divya-2130 <divyasriyadava@gmail.com>
2024-11-04 15:07:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navigation from "./Navigation/Navigation";
-import Signup from "./signup/signup";
+import Signup from "./Signup/Signup";
 import Login from "./Login/Login";
 import AddProduct from "./AddProduct/AddProduct";
 import Home from "./Home/Home";
